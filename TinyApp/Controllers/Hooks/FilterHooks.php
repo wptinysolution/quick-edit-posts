09c8d39bf834f575c51f4e07fe84e8f628deb7f0
Mamunur Rashid <rmamunur105@gmail.com>
2023-12-25 22:45:50 +0600

    fixed error

--- TinyApp/Controllers/Hooks/FilterHooks.php
+++ TinyApp/Controllers/Hooks/FilterHooks.php
@@ -21,41 +21,40 @@ class FilterHooks {
 	 * @return void
 	 */
 	public static function init_hooks() {
-        // Plugins Setting Page.
-        add_filter( 'plugin_action_links_' . PQE_BASENAME,  [ __CLASS__, 'plugins_setting_links' ] );
-		add_filter('plugin_row_meta', [ __CLASS__, 'plugin_row_meta' ], 10, 2);
+		/*
+		// Plugins Setting Page.
+		// add_filter( 'plugin_action_links_' . PQE_BASENAME, [ __CLASS__, 'plugins_setting_links' ] );
+		*/
+		add_filter( 'plugin_row_meta', [ __CLASS__, 'plugin_row_meta' ], 10, 2 );
 	}
 
-    /**
-     * @param array $links default plugin action link
-     *
-     * @return array [array] plugin action link
-     */
-    public static function plugins_setting_links( $links ) {
-        $links['mediaedit_settings'] = '<a href="' . admin_url( 'admin.php?page=pqe-admin' ) . '">' . esc_html__( 'Start Editing', 'pqe' ) . '</a>';
-        /*
-         * TODO:: Next Version
-         *
-         */
-        if( ! Fns::is_plugins_installed('media-library-tools-pro/media-library-tools-pro.php') ){
-            // $links['pqe_pro'] = sprintf( '<a href="#" target="_blank" style="color: #39b54a; font-weight: bold;">' . esc_html__( 'Go Pro', 'wp-media' ) . '</a>' );
-        }
-        return $links;
-    }
 	/**
-	 * @param $links
-	 * @param $file
+	 * @param array $links default plugin action link.
+	 *
+	 * @return array [array] plugin action link
+	 */
+	/**
+		//public static function plugins_setting_links( $links ) {
+		//	$links['mediaedit_settings'] = '<a href="' . admin_url( 'admin.php?page=pqe-admin' ) . '">' . esc_html__( 'Start Editing', 'pqe' ) . '</a>';
+		//	//if ( ! Fns::is_plugins_installed( 'media-library-tools-pro/media-library-tools-pro.php' ) ) {
+		//	//	// $links['pqe_pro'] = sprintf( '<a href="#" target="_blank" style="color: #39b54a; font-weight: bold;">' . esc_html__( 'Go Pro', 'wp-media' ) . '</a>' );
+		//	//}
+		//	return $links;
+		//}
+	 **/
+	/**
+	 *
+	 * @param string $links LINKS.
+	 * @param string $file file path.
 	 *
 	 * @return array
 	 */
-	public static function plugin_row_meta($links, $file) {
-		if ( $file == PQE_BASENAME ) {
-			$report_url = 'https://www.wptinysolutions.com/contact' ;//home_url( '/wp-admin/upload.php?page=tsmlt-media-tools' );
-			$row_meta['issues'] = sprintf('%2$s <a target="_blank" href="%1$s">%3$s</a>', esc_url($report_url), esc_html__('Facing issue?', 'tsmlt-media-tools'), '<span style="color: red">' . esc_html__('Please open a support ticket.', 'tsmlt-media-tools') . '</span>');
-			return array_merge($links, $row_meta);
+	public static function plugin_row_meta( $links, $file ) {
+		if ( PQE_BASENAME === $file ) {
+			$report_url         = 'https://www.wptinysolutions.com/contact';
+			$row_meta['issues'] = sprintf( '%2$s <a target="_blank" href="%1$s">%3$s</a>', esc_url( $report_url ), esc_html__( 'Facing issue?', 'tsmlt-media-tools' ), '<span style="color: red">' . esc_html__( 'Please open a support ticket.', 'tsmlt-media-tools' ) . '</span>' );
+			return array_merge( $links, $row_meta );
 		}
-		return (array)$links;
+		return (array) $links;
 	}
-
 }
-
