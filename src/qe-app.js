a522aef2dfb86db20cf9d57e1ca4dbb0eb9172ef
Mamunur Rashid <rmamunur105@gmail.com>
2023-12-27 01:53:39 +0600

    Need remove some content

--- src/qe-app.js
+++ src/qe-app.js
@@ -1,18 +1,37 @@
 ;(function ($) {
-
-    jQuery(document).ready( ($) => {
+    jQuery(document).ready(function ($) {
         // Add a click event listener to the button with the class 'edit-button'.
-        $('#wpbody').on('click', '.edit-button', function(event) {
+        $('#wpbody').on('click', '.edit-button', function (event) {
             event.preventDefault();
-            // Calculate the center position
-            var width = 1000, height = 600;
-            var left = (window.outerWidth - width) / 2;
-            var top = (window.outerHeight - height) / 2;
+
             // Get the URL from the 'data-url' attribute.
             var url = $(this).data('url');
-            // Open the URL in a new window or tab.
-            window.open(url, '_blank', 'width=' + width + ',height=' + height + ',left=' + left + ',top=' + top);
+
+            // Create a new popup window
+            var popupWindow = window.open('', '_blank', 'width=1000,height=600');
+
+            // Fetch the content of the page with id "adminmenuwrap" using AJAX
+            $.ajax({
+                url: url,
+                dataType: 'html',
+                success: function (data) {
+                    // Remove the content of the adminmenuwrap section
+                    // var data = $(data).find('#adminmenuwrap').remove();
+
+                    // Write the remaining content into the popup window document
+                    popupWindow.document.write(data);
+                    // Add a load event listener to the popup window's document
+                    // setTimeout(function () {
+                    //
+                    //     console.log( 'Hello' );
+                    //     // Remove the content of the adminmenuwrap section
+                    //     popupWindow.document.body.find('#adminmenuwrap').remove();
+                    //     // Close the document to finish writing
+                    //     popupWindow.document.close();
+                    // }, 1000); // You can adjust the delay time as needed
+
+                }
+            });
         });
     });
-
 })(jQuery);
