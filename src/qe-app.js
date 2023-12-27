16f0dd50027e71ad7f3e8454ecbb5bf5b8475df7
Mamunur Rashid <rmamunur105@gmail.com>
2023-12-28 02:19:01 +0600

    Fixed multiple window issue

--- src/qe-app.js
+++ src/qe-app.js
@@ -1,25 +1,36 @@
 ;(function ($) {
     jQuery(document).ready(function ($) {
+        // Initialize a variable to store the reference to the opened window
+        let popupWindow = null;
+
         // Add a click event listener to the button with the class 'edit-button'.
         $('#wpbody').on('click', '.edit-button', function (event) {
             event.preventDefault();
-            // Get the URL from the 'data-url' attribute.
-            var url = $(this).data('url') + '&window=open_window';
-            // Create a new popup window
-            var popupWindow = window.open( url, '_blank', 'width=1000,height=600' );
-            setTimeout(function () {
-                var form = $(popupWindow.document.body).find('#post').html();
-                $(popupWindow.document.body).find('#adminmenumain').remove();
+            // Check if the window is already open
+            if (popupWindow && !popupWindow.closed) {
+                // If open, focus on the existing window instead of opening a new one
+                popupWindow.focus();
+            } else {
+                // Get the URL from the 'data-url' attribute.
+                var url = $(this).data('url') + '&window=open_window';
+                // Create a new popup window
+                popupWindow = window.open( url, '_blank', 'width=1000,height=600' );
+                setTimeout(function () {
+                    var form = $(popupWindow.document.body).find('#post').html();
+                    $(popupWindow.document.body).find('#adminmenumain').remove();
 
-                $(popupWindow.document.body).find('#woocommerce-embedded-root').remove();
-                $(popupWindow.document.body).find('#wpcontent').css({
-                    margin: 0,
-                });
-                form = '<form name="post" action="post.php" method="post" id="post"> ' + form + ' </form>';
-                $(popupWindow.document.body).find('#wpbody-content').html(form);
-                $(popupWindow.document.body).removeClass('opacity-0');
-                popupWindow.document.close();
-            }, 1000); // You can adjust the delay time as needed
+                    $(popupWindow.document.body).find('#woocommerce-embedded-root').remove();
+                    $(popupWindow.document.body).find('#wpcontent').css({
+                        margin: 0,
+                    });
+                    form = '<form name="post" action="post.php" method="post" id="post"> ' + form + ' </form>';
+                    $(popupWindow.document.body).find('#wpbody-content').html(form);
+                    $(popupWindow.document.body).removeClass('opacity-0');
+                    popupWindow.document.close();
+                }, 1000); // You can adjust the delay time as needed
+
+            }
+
         });
     });
 
