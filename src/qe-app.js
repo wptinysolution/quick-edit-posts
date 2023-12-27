dff4d3a75ee336e06ddb16de02bf6584db399b0f
Mamunur Rashid <rmamunur105@gmail.com>
2023-12-28 02:30:13 +0600

    Fixed error

--- src/qe-app.js
+++ src/qe-app.js
@@ -1,8 +1,24 @@
 ;(function ($) {
     jQuery(document).ready(function ($) {
+
+        setTimeout(function () {
+            const isWindowOpen = window.location.search.includes('window=open_window');
+            if( isWindowOpen ) {
+                var form = $(document.body).find('#post').html();
+                $(document.body).find('#adminmenumain').remove();
+
+                $(document.body).find('#woocommerce-embedded-root').remove();
+                $(document.body).find('#wpcontent').css({
+                    margin: 0,
+                });
+                form = '<form name="post" action="post.php" method="post" id="post"> ' + form + ' </form>';
+                $(document.body).find('#wpbody-content').html(form);
+                $(document.body).removeClass('opacity-0');
+            }
+        }, 1000); // You can adjust the delay time as needed
+
         // Initialize a variable to store the reference to the opened window
         let popupWindow = null;
-
         // Add a click event listener to the button with the class 'edit-button'.
         $('#wpbody').on('click', '.edit-button', function (event) {
             event.preventDefault();
